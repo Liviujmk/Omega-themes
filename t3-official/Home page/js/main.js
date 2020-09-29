08d07b415ff871c4571d1c34b5d1f10e5e40b832
Liviujmk <64259428+Liviujmk@users.noreply.github.com>
2020-09-29 17:25:57 +0300

    Add files via upload

---
+++ t3-official/Home page/js/main.js
@@ -0,0 +1,17 @@
+// Add active class to the current button (highlight it)
+var btns = document.getElementsByClassName("nav__link");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
+function topnav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
